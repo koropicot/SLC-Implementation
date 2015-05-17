d240269e59512bae1462f798eb9d0ebd87f5a9d5
koropicot <koropicot@gmail.com>
2015-05-18 00:30:06 +0900

    FsLexYaccでSLCのプレパーサーを実装(A.1)

---
+++ SLCImplementation/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+open System
+open form
+open Lexer
+open Parser 
+open Microsoft.FSharp.Text.Lexing
+
+//string -> tokens -> form
+let parse = LexBuffer<_>.FromString >> Parser.start Lexer.token
+
+[<EntryPoint>]
+let main args =
+    Console.ReadLine() |> parse |> Console.WriteLine 
+    0
---
+++ SLCImplementation/form.fs
@@ -0,0 +1,20 @@
+﻿//syntactic forms recognized by the YACC pre-parser (pp.103-104)
+module form
+
+type form =
+    | SNum of int
+    | SIde of string
+    | SAdd of form * form
+    | SSub of form * form
+    | SMul of form * form
+    | SUp of form * form
+    | SDwn of form * form
+    | SEql of form * form
+    | SLft of form * form
+    | SRgt of form * form
+    | SRec of form * form
+    | SPar of form list
+    | SBrc of form list
+// SCLの構文
+//    | SSqb of form list
+//    | SAnb of form list

5bcab2a57a240f99cc2f8604cb95d7a9239bb1d7
koropicot <koropicot@gmail.com>
2015-05-19 09:00:48 +0900

    implement repl

--- SLCImplementation/Program.fs
+++ SLCImplementation/Program.fs
@@ -4,11 +4,36 @@ open form
 open Lexer
 open Parser 
 open Microsoft.FSharp.Text.Lexing
+open toplev
 
 //string -> tokens -> form
-let parse = LexBuffer<_>.FromString >> Parser.start Lexer.token
+let pre_parse = LexBuffer<_>.FromString >> Parser.start Lexer.token
+
+//REPL
+let rec repl () =
+    printf ">"
+    match Console.ReadLine().Trim().Split(' ', '\t') |> List.ofArray |> List.filter ((<>) "") with
+    | ["quit"] -> ()
+    | ["help"] | ["h"] ->
+        printfn "Usage:"
+        printfn "    >expression"
+        printfn "        evaluate expression"
+        printfn "    >def name expression"
+        printfn "        evaluate expression and define it"
+        printfn "    >quit"
+        printfn "        quit repl"
+        repl ()
+    | "def"::x::((_::_) as s)->
+        try zd x (pre_parse (List.reduce (+) s)) with ex -> printfn "Error: %s" ex.Message
+        repl ()
+    | _::_ as s ->
+        try z (pre_parse (List.reduce (+) s)) with ex -> printfn "Error: %s" ex.Message
+        repl ()
+    | _ ->
+        printfn "show usage: >help"
+        repl ()
 
 [<EntryPoint>]
 let main args =
-    Console.ReadLine() |> parse |> Console.WriteLine 
+    repl ()
     0

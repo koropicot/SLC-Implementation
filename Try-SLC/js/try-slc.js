d37504fb57251ee852ec6aecb1b42ad5b7669221
koropicot <koropicot@gmail.com>
2015-06-06 06:51:38 +0900

    Fix Shif+Enter event for Firefox

--- Try-SLC/js/try-slc.js
+++ Try-SLC/js/try-slc.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 				this.selectionEnd = s + 1;
 			}
 		}
-		if(event.shiftKey){
+		if(e.shiftKey){
 			if((e.keyCode || e.which) == 13){
 				e.preventDefault();
 				onSubmit();

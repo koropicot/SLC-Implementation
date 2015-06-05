1cb5559791179442770aeb8615ebe8b3ba08a3ae
koropicot <koropicot@gmail.com>
2015-06-05 23:15:04 +0900

    Add compiled js output

--- SLCImplementation/Program.fs
+++ SLCImplementation/Program.fs
@@ -4,6 +4,8 @@ open System
 open form
 open preparse
 open toplev
+open FunScript
+open FunScript.TypeScript
 
 //REPL
 let rec repl () =
@@ -29,7 +31,33 @@ let rec repl () =
             printfn "show usage: >help"
         repl ()
 
+
+
+
+//JSFFI
+[<JSEmit("return createOutputCard({0}, {1}, {2}, {3})")>]
+let createOutputCard (code: string) (output: string) (error: bool) (name: string): 'TAny = failwith "never"
+
+let submit s = 
+    try
+        match pre_parse s with
+        | Def (n, e) -> createOutputCard s (zd_to_str n e) false n
+        | Eval e -> createOutputCard s (z_to_str e) false ""
+    with ex ->
+        createOutputCard s ("Error: " + ex.Message) true ""
+
+#if TOJS
+[<EntryPoint>]
+let main args =
+    let sw = new IO.StreamWriter("slc.js")
+    Compiler.compile(<@ submit @>) |> sprintf "function getSubmit(){\n%s;\n}" |> sw.Write
+    sw.Close()
+    0
+#else
 [<EntryPoint>]
 let main args =
     repl ()
     0
+#endif
+
+
